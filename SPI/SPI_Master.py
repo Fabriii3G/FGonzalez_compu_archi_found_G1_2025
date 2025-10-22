5a50f796241271cc36ad487f0b7c8d35ba8ebd5e
Fabriii3G <fabgonzalez@estudiantec.cr>
2025-10-22 13:37:53 -0600

    master basico, que envia un bit

---
+++ SPI/SPI_Master.py
@@ -0,0 +1,31 @@
+from machine import Pin, SPI
+import time
+
+# Configuración SPI
+# SPI(0) usa los pines por defecto:
+# SCK = GP18, MOSI = GP19, MISO = GP16
+# (puedes cambiarlo según tu conexión)
+spi = SPI(0,
+          baudrate=1000000,  # 1 MHz
+          polarity=0,
+          phase=0,
+          bits=8,
+          firstbit=SPI.MSB,
+          sck=Pin(18),
+          mosi=Pin(19),
+          miso=Pin(16))
+
+# Pin Chip Select (CS)
+cs = Pin(17, Pin.OUT)
+cs.value(1)  # CS inactivo (alto)
+
+# --- Envío de un byte por SPI ---
+data = bytes([0b10101010])  # Byte a enviar
+
+while True:
+    cs.value(0)          # Seleccionar esclavo
+    spi.write(data)      # Enviar byte
+    cs.value(1)          # Liberar esclavo
+
+    print("Dato enviado:", bin(data[0]))
+    time.sleep(1)        # Espera 1 segundo antes de enviar nuevamente
